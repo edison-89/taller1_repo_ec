6ac97efba622ce97a567fa147ed261477c8682cf
Edison Cayambe <edisonc_89@hotmail.com>
2022-05-17 19:55:28 -0500

    Segundo cambio solicitado

--- src/taller1_repo_ec/Main.java
+++ src/taller1_repo_ec/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi primer proyecto JAVA en GitHub");
+		System.out.println("Mi segundo comentario");
 	}
 
 }
